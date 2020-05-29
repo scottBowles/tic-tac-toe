d0d3db9731f3ee221693aa05dea3b28ad7545ea4
Scott Bowles <shbowles@gmail.com>
2020-05-29 17:13:07 -0400

    Initial commit

---
+++ main.js
@@ -0,0 +1,8 @@
+const ui = (() => {
+  const gameboard = document.querySelector("#gameboard");
+  const newGameButton = document.querySelector("#newGameButton");
+  const playerOne = document.querySelector("#playerOne");
+  const playerTwo = document.querySelector("#playerTwo");
+  const message = document.querySelector("#message");
+  return {};
+})();
