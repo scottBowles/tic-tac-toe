b478cf7eb7cc4dc0a3d2442096fc81e67896cac3
Scott Bowles <shbowles@gmail.com>
2020-06-01 17:02:40 -0400

    Set up gameboard and start building modules

--- main.js
+++ main.js
@@ -6,3 +6,54 @@ const ui = (() => {
   const message = document.querySelector("#message");
   return {};
 })();
+
+const gameboard = (() => {
+
+  const currentState = [undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined];
+
+  function newPlay(mark, place) {
+    currentState[place] = mark;
+  };
+
+  function clearBoard() {
+    currentState = [undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined];
+  };
+  
+  function checkForWinner() {
+
+  };
+  
+  function checkForTie() {
+
+  };
+  
+  return {
+    newPlay,
+    clearBoard,
+    checkForWinner,
+    checkForTie,
+  };
+
+})();
+
+const gameplay = (() => {
+
+  const turn = 'x';
+
+  function nextTurn() { turn === 'x' ? turn = 'o' : turn = 'x' };
+
+  function checkEndConditions() { return checkForWinner() || checkForTie() }
+
+  function newGame() {
+    gameboard.clearBoard();
+    ui.clearBoard();
+    // Still need to finish this
+  }
+
+  return {
+    nextTurn,
+    checkEndConditions,
+    newGame,
+  };
+
+})();
